41bc360f882b366207bd0e6946670be2e581e95a
newowner <tylercorreu@yahoo.com>
2023-02-11 10:51:33 -0600

    initial commit

---
+++ src/app/pokemon/pokemon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-pokemon',
+  templateUrl: './pokemon.component.html',
+  styleUrls: ['./pokemon.component.css']
+})
+export class PokemonComponent {
+  
+}
