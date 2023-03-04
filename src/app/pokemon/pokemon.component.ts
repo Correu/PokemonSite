66dbefd6a85766118d9d7df67be22db28defb24a
newowner <tylercorreu@yahoo.com>
2023-03-04 13:20:24 -0600

    pokemon updates, began init build team creation

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -9,17 +9,32 @@ import { PokemonServiceService } from '../services/pokemon-service.service';
 export class PokemonComponent implements OnInit {
   pokemonName:string = '';
   pokemon: any[] = [];
+  moves: any[] = [];
   constructor(private pokemonService: PokemonServiceService) { }
   
   ngOnInit(): void {
-    
+    this.buildTeam();
   }
 
   searchForPokemon(name: string) {
     this.pokemon = [];
+    this.moves = [];
+    this.pokemonName = '';
     this.pokemonService.getPokemon(name).subscribe(res => {
       this.pokemon = res.abilities;
-      console.log(this.pokemon);
+      this.moves = res.moves;
+      this.pokemonName = res.name;
+      console.log(this.pokemon + " " + this.moves + this.pokemonName);
+      window.alert(this.pokemon);
     });
+  }
+
+  buildTeam() {
+    this.pokemon = [];
+    this.pokemonService.getPokemon("1").subscribe(res => {
+      this.pokemon = res.abilities;
+      console.log("Testing init build team.");
+      console.log(this.pokemon);
+    })
   }
 }
