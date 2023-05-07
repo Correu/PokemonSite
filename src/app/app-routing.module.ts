2a72372fb036d2288cd55bedabeecc2c968b57b0
newowner <tylercorreu@yahoo.com>
2023-05-07 15:02:04 -0500

    changes to router to have only one header.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
 
 const routes: Routes = [
