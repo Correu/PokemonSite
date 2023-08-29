9bfbf2880a5e7cee35118767ee3abd124d6e64f4
Tyler Correu <60193729+Correu@users.noreply.github.com>
2023-08-28 21:52:22 -0500

    battle component scope additions

--- src/app/components/battle/battle.component.ts
+++ src/app/components/battle/battle.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-battle',
   templateUrl: './battle.component.html',
   styleUrls: ['./battle.component.css']
 })
-export class BattleComponent {
 
+@Injectable()
+export class BattleComponent implements OnInit {
+
+  //two random teams of 6 pokemon
+  //each pokemon has a level and a 
+  //each pokemon has 4 moves random up to the current level they are at
+  //turn based
+  //
+
+  ngOnInit(): void {
+    //methods to initialize
+  }
 }
