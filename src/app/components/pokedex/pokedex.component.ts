c9b7d6c4db13f2322ea0d95cde59b91836f879ee
Tyler Correu <60193729+Correu@users.noreply.github.com>
2023-09-07 23:07:27 -0500

    updates

--- src/app/components/pokedex/pokedex.component.ts
+++ src/app/components/pokedex/pokedex.component.ts
@@ -1,9 +1,11 @@
 import { Component, Injectable, OnInit } from '@angular/core';
 import { PokemonService } from 'src/app/services/pokemon.service';
+import { Pokemon } from 'src/app/interfaces/pokemon';
+
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
-  styleUrls: ['./pokedex.component.css']
+  styleUrls: ['./pokedex.component.css'],
 })
 
 @Injectable()
@@ -12,42 +14,14 @@ export class PokedexComponent implements OnInit{
   //more information about the pokemon in the pages.
   
   pokemon: any[] = [];
-  pokemonList: Array<Pokemon> = [];
+  pokemonList: Pokemon[] = [];
   pokedex: Pokemon[] = [];
 
   constructor(private pokemonService: PokemonService) { }
 
   ngOnInit(): void {
     //methods to initialize
-    this.getPokedex();
+    this.pokedex = this.pokemonService.getPokedexList();
   }
 
-  getPokedex() {
-    this.pokemonService.getPokedex().subscribe(pokemon => {
-      // for (let index = 0; index < res.results.length; index++) {
-      //   const element = res.results[index];
-      //   this.pokemonService.getPokemon(element.url).subscribe(resource => {
-      //     this.pokemonList.push({pokemonId: resource.id, pokemonName: resource.name, pokemonUrl: element.url, pokemonImageUrl: resource.sprites.back_default})
-      //   });
-      // }
-      pokemon.results.forEach((element : any) => {
-        const poke: Pokemon = {
-          pokemonName: element.name,
-          pokemonUrl: element.url
-        }
-  
-        this.pokedex.push(poke);
-      }); 
-
-      console.log(pokemon);
-      console.log(this.pokedex);
-    });
-  }
-}
-
-
-
-type Pokemon = {
-  pokemonName: string;
-  pokemonUrl: string;
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Pokemon, Sprite } from '../interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +9,53 @@ import { Observable } from 'rxjs';
 
 export class PokemonService {
 
-  constructor(private http:HttpClient) { }
+  pokemonIds: PokeIds[] = [];
+  pokedex: Pokemon[] = [];
+
+  constructor(private http:HttpClient) {
+    this.getPokedex().subscribe(res => {
+      console.log(res);
+      this.pokemonIds = res.results;
+    });
+
+    console.log(this.pokemonIds);
+
+    this.pokemonIds.forEach(id => {
+      this.getPokemon(id.url).subscribe(p => {
+
+        const sprite: Sprite = {
+          frontDefault: p.sprites.front_default,
+          backDefault: p.sprites.back_default,
+          frontShiny: p.sprites.front_default_shiny,
+          backShiny: p.sprites.back_shiny,
+        };
+
+        const pokemon: Pokemon = {
+          id: p.id,
+          name: p.name,
+          baseExperience: p.base_experience,
+          height: p.height,
+          order: p.order,
+          weight: p.weight,
+          abilities: p.ability,
+          forms: p.forms,
+          heldItems: p.item,
+          moves: p.moves.move,
+          sprites: sprite,
+          stats: p.stat
+        };
+
+        this.pokedex.push(pokemon);
+      })
+    })
+   }
 
   public getPokemon(name:string): Observable<any> {
     return this.http.get(name);
   }
 
   public getPokemonById(id: string): Observable<any> {
-    let url = 'https://pokeapi.co/api/v2/pokemon' + id;
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + id;
     console.log(url);
     return this.http.get(url);
   }
@@ -24,4 +64,13 @@ export class PokemonService {
     let url = 'https://pokeapi.co/api/v2/pokemon/?limit=100';
     return this.http.get(url);
   }
+
+  public getPokedexList() {
+    return this.pokedex;
+  }
+}
+
+type PokeIds = {
+  name: string,
+  url: string;
 }
