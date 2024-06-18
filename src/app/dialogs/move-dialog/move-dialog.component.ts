6fe20258fbd1d2c5eeed0b008efdb5865d9e7dd2
Tyler Correu <60193729+Correu@users.noreply.github.com>
2024-06-18 06:49:46 -0500

    added new dialogs

---
+++ src/app/dialogs/move-dialog/move-dialog.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Move } from 'src/app/interfaces/pokemon';
+import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
+
+@Component({
+  selector: 'app-move-dialog',
+  templateUrl: './move-dialog.component.html',
+  styleUrls: ['./move-dialog.component.css']
+})
+export class MoveDialogComponent {
+  
+  moveInfo!: Move;
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {name: string}, private pokemonService: PokemonService) {
+
+  }
+
+  ngOnInit(): void {
+    this.pokemonService.getMove(this.data.name).subscribe((res: any) => {
+      this.moveInfo = res;
+      console.log(res);
+    })
+  } 
+}
---
+++ src/app/dialogs/pokedex-instruction-dialog/pokedex-instruction-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokedex-instruction-dialog',
+  templateUrl: './pokedex-instruction-dialog.component.html',
+  styleUrls: ['./pokedex-instruction-dialog.component.css']
+})
+export class PokedexInstructionDialogComponent {
+
+}
